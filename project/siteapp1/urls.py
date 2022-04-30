e96fddad861fcf603ed9728295642e2bd413e9cf
ShadowPapaya <100354004+ShadowPapaya@users.noreply.github.com>
2022-04-30 12:12:03 +0530

    landing n home
    front end integrated but storing data from form is left.

--- project/siteapp1/urls.py
+++ project/siteapp1/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from siteapp1 import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.index , name="home"),
-    path('MyReports', views.MyReports , name="MyReports")
+    path('home', views.index , name="home"),
+    path('MyReports', views.MyReports , name="MyReports"),
+    path('landing', views.landing, name="landing")
 ]
