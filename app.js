b161ad7876e96bb902a4acb2bd3b7501ee5b1818
Vayansh <vgarg3_be21@thapar.edu>
2022-05-01 02:08:17 +0530

    some-changes
    navbar bug removed and gradient added to homepage.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function my(){
       
     var oh= document.getElementById('reports').getBoundingClientRect().height;
     document.getElementById('background').setAttribute("style",`height:${oh}px;`);
-    }
+    document.getElementById('report-back').setAttribute("style",`height: ${oh}px;`)    
+}
 const d = new Date(); 
 document.getElementById('date').innerHTML= "Date: "+d.getDay() + "-" +d.getMonth() + "-" + d.getFullYear();
